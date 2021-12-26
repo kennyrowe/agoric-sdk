ef116ca53b2ad131a38495390791da166a2fedee
Mark S. Miller <erights@gmail.com>
2021-12-26 12:12:43 -0800

    fix: zoe start using patterns

--- packages/zoe/src/typeGuards.js
+++ packages/zoe/src/typeGuards.js
@@ -1,9 +1,38 @@
 // @ts-check
 
-/**
- * @param {ExitRule} exit
- * @returns {exit is OnDemandExitRule}
- */
+import { AmountPattern } from '@agoric/ertp';
+import { M } from '@agoric/store';
+
+export const ExitOnDemandPattern = harden({ onDemand: null });
+
+export const ExitWaivedPattern = harden({ waived: null });
+
+export const ExitAfterDeadlinePattern = harden({
+  afterDeadline: { timer: M.remotable(), deadline: M.nat() },
+});
+
+export const ExitRulePattern = M.or(
+  ExitOnDemandPattern,
+  ExitWaivedPattern,
+  ExitAfterDeadlinePattern,
+);
+
+export const KeywordRecordPatternOf = valuePatt =>
+  M.recordOf(M.string(), valuePatt);
+
+export const AmountKeywordRecordPattern = KeywordRecordPatternOf(AmountPattern);
+
+export const PatternKeywordRecordPattern = KeywordRecordPatternOf(M.pattern());
+
+export const ProposalPattern = M.partial(
+  {
+    want: M.or(undefined, PatternKeywordRecordPattern),
+    give: M.or(undefined, AmountKeywordRecordPattern),
+    exit: M.or(undefined, ExitRulePattern),
+  },
+  {},
+);
+
 export const isOnDemandExitRule = exit => {
   const [exitKey] = Object.getOwnPropertyNames(exit);
   return exitKey === 'onDemand';
